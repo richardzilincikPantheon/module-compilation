09ff3d7ec167426737c16dfa3bc8517cc6557ce3
SlavomirMazurPantheon <67378888+SlavomirMazurPantheon@users.noreply.github.com>
2022-08-01 10:52:20 +0200

    test: unit tests covering yang_version_1_1.py (#182)
    Signed-off-by: Slavomir Mazur <slavomir.mazur@pantheon.tech>

--- bin/yang_version_1_1.py
+++ bin/yang_version_1_1.py
@@ -15,77 +15,78 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+__author__ = 'Benoit Claise'
+__copyright__ = 'Copyright(c) 2015-2018, Cisco Systems, Inc., Copyright The IETF Trust 2019, All Rights Reserved'
+__license__ = 'Apache License, Version 2.0'
+__email__ = 'bclaise@cisco.com'
+
 import argparse
 import os
+import shutil
+import subprocess
 
 from create_config import create_config
 from remove_directory_content import remove_directory_content
 
-__author__ = 'Benoit Claise'
-__copyright__ = "Copyright(c) 2015, Cisco Systems, Inc.,  Copyright The IETF Trust 2019, All Rights Reserved"
-__license__ = "Apache v2.0"
-__email__ = "bclaise@cisco.com"
 
-
-def YANGversion11(src_path, dest_path, debug):
+def find_v11_models(src_dir: str, dst_dir: str, debug: int = 0) -> list:
     """
-    YANG 1.1 Processing Tool.
-    This is the main (external) API entry for the module.
-    The functions copies all YANG 1.1 modules to a yangpath
-    :param src_path: The optional directory where to find the source YANG data models
-    :param dest_path: The path to store the version 1.1 YANG data models
-    :param debug: Determines how much debug output is printed to the console
-    """ 
+    This method will copy all yang models of version 1.1
+    from directory 'src_dir' to directory 'dst_dir'.
 
-    if debug > 0:
-        print("No test condition")
-    
-    remove_directory_content(dest_path, debug)
-    yang_model_list = [f for f in os.listdir(src_path) if os.path.isfile(os.path.join(src_path, f))]
+    Arguments:
+        :param src_dir      (str) directory where to find the source YANG models files
+        :param dst_dir      (str) directory where to store version 1.1 YANG models
+        :param debug_level  (int) debug level; If > 0 print some debug statements to the console
+    """
+    if not os.path.isdir(src_dir):
+        return []
+    remove_directory_content(dst_dir, debug)
+    yang_model_list = [f for f in os.listdir(src_dir) if os.path.isfile(os.path.join(src_dir, f))]
     yang_model_list_v11 = []
-    
-    for yang_model in yang_model_list:
-        bash_command = "grep yang-version {} | grep 1.1".format(os.path.join(src_path, yang_model) )
-        if debug > 0:
-            print("bash command: " + bash_command)
-        temp_result = os.popen(bash_command).read()
-        if "1.1" in temp_result:
-            if debug > 0:
-                print("DEBUG: " + yang_model + " is version 1.1 ")
-            yang_model_list_v11.append(yang_model)
-            # copy function below could be improved with python command, as opposed to a bash
-            bash_command = "cp  {} {}".format(os.path.join(src_path, yang_model), os.path.join(dest_path, yang_model))
-            temp_result = os.popen(bash_command).read()
-    if debug > 0:
-        print("DEBUG: YANG Model list with YANG version 1.1: " )
-        if yang_model_list_v11:
-            print(yang_model_list_v11)
-        else:
-            print("  No YANG modules version 1.1")
 
- 
-# ----------------------------------------------------------------------
-# Main
-# ----------------------------------------------------------------------
+    for yang_model in yang_model_list:
+        src_file_path = os.path.join(src_dir, yang_model)
+        bash_command = 'grep yang-version {} | grep 1.1'.format(src_file_path)
+        if debug > 0:
+            print('DEBUG: grep command: {}'.format(bash_command))
+        grep_result = subprocess.run(bash_command, shell=True, capture_output=True, check=False).stdout.decode()
+        if '1.1' not in grep_result:
+            continue
+        if debug > 0:
+            print('DEBUG: {} is version 1.1 '.format(yang_model))
+        yang_model_list_v11.append(yang_model)
+        dst_file_path = os.path.join(dst_dir, yang_model)
+        shutil.copy2(src_file_path, dst_file_path)
+    if debug > 0:
+        print('DEBUG: list of YANG models with version 1.1:\n{}'.format(yang_model_list_v11))
+    return yang_model_list_v11
 
 
 if __name__ == '__main__':
     config = create_config()
     ietf_directory = config.get('Directory-Section', 'ietf-directory')
 
-    src_path = os.path.join(ietf_directory, 'YANG')
+    src = os.path.join(ietf_directory, 'YANG')
+    dst = os.path.join(ietf_directory, 'YANG-v11')
 
     parser = argparse.ArgumentParser(description='YANG 1.1 Processing Tool. Copy all YANG 1.1 modules to destpath')
-    parser.add_argument('--destpath',
-                        help='Path to store the version 1.1 YANG data models. '
-                             'Default is "{}/YANG-v11/"'.format(ietf_directory),
+    parser.add_argument('--srcpath',
+                        help='Directory where find YANG models. '
+                             'Default is "{}"'.format(src),
                         type=str,
-                        default='{}/YANG-v11/'.format(ietf_directory))
+                        default=src)
+    parser.add_argument('--dstpath',
+                        help='Directory where to store version 1.1 YANG models. '
+                             'Default is "{}"'.format(dst),
+                        type=str,
+                        default=dst)
     parser.add_argument('--debug',
                         help='Debug level - default is 0',
                         type=int,
                         default=0)
 
     args = parser.parse_args()
-        
-    YANGversion11(src_path, args.destpath,args.debug)
+
+    find_v11_models(args.srcpath, args.dstpath, args.debug)
---
+++ tests/test_yang_version_1_1.py
@@ -0,0 +1,68 @@
+# Copyright The IETF Trust 2022, All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+__author__ = 'Slavomir Mazur'
+__copyright__ = 'Copyright The IETF Trust 2022, All Rights Reserved'
+__license__ = 'Apache License, Version 2.0'
+__email__ = 'slavomir.mazur@pantheon.tech'
+
+import os
+import shutil
+import subprocess
+import unittest
+
+import yang_version_1_1 as yv11
+
+
+class TestYangVersion11(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.resource_path = os.path.join(os.environ['VIRTUAL_ENV'], 'tests/resources/yang_version_1_1')
+        self.script_path = os.path.join(os.environ['VIRTUAL_ENV'], 'bin/yang_version_1_1.py')
+        self.src = os.path.join(self.resource_path, 'YANG')
+        self.dst = os.path.join(self.resource_path, 'YANG-v11')
+
+    def setUp(self) -> None:
+        shutil.rmtree(self.dst, ignore_errors=True)
+
+    def test_yang_version_1_1(self):
+        """ Find and copy the yang files that contain 'yang-version 1.1' string. """
+        result = yv11.find_v11_models(self.src, self.dst, 1)
+        self.assertNotEqual(result, [])
+        self.assertIn('test.yang', result)
+        self.assertNotIn('test2.yang', result)
+
+        v1_files = os.listdir(self.dst)
+        self.assertNotEqual(v1_files, [])
+        self.assertIn('test.yang', v1_files)
+
+    def test_yang_version_1_1_src_not_exists(self):
+        """ Test the case when the src directory does not exist. """
+        result = yv11.find_v11_models('', self.dst)
+        self.assertEqual(result, [])
+
+    def test_yang_version_1_1_from_console(self):
+        """ Run the script from the console by passing the arguments. """
+        bash_command = 'python {} --srcpath {} --dstpath {} --debug 1'.format(self.script_path, self.src, self.dst)
+        subprocess.run(bash_command, shell=True, capture_output=True, check=False).stdout.decode()
+
+        v1_files = os.listdir(self.dst)
+        self.assertNotEqual(v1_files, [])
+        self.assertIn('test.yang', v1_files)
+
+
+if __name__ == '__main__':
+    unittest.main()
